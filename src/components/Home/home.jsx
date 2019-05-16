9dd3e987557317b2a0ae0bead50e75302d64108a
vla15 <vincela15@gmail.com>
2019-05-15 22:13:06 -0700

    add spinner while image loads in full

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -4,6 +4,47 @@ import './home.css';
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoading: true
+    }
+    this.backgroundImageRef = null;
+    this.setBackgroundImageRef = element => {
+      this.backgroundImageRef = element
+    }
+  }
+
+  componentDidMount() {
+    //checks if image is loaded
+    //if loaded remove spinner and fade in image
+    if (this.backgroundImageRef.style.backgroundImage) {
+      const url = this.backgroundImageRef.style.backgroundImage.match(/\((.*?)\)/)[1].replace(/('|")/g, '');
+      const image = new Image();
+      image.onload = () => {
+        this.backgroundImageRef.animate([
+          { // from
+            opacity: 0,
+          },
+          { // to
+            opacity: 1,
+          }
+        ], {duration: 500, easing: 'linear'})
+        this.setState({ isLoading: false })
+      }
+      image.src = url;
+      if (image.complete) {
+        image.onload();
+      }
+    }
+  }
+
+  generateSpinner() {
+    return (
+      <div className="spinner-container">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    )
   }
 
   render() {
@@ -18,8 +59,9 @@ class Home extends Component {
     };
     return (
       <div className="home-body">
-        <div className="mobile-home-body-splash" style={style}></div>
-        <img className="desktop-home-body-splash" src={`${process.env.PUBLIC_URL}/splash.jpg`}/>
+        {this.state.isLoading ? this.generateSpinner() : '' }
+        <div ref={this.setBackgroundImageRef} className="mobile-home-body-splash fade-in" style={style}></div>
+        <img className="desktop-home-body-splash fade-in" src={`${process.env.PUBLIC_URL}/splash.jpg`}/>
       </div>
     );
   }
