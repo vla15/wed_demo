0473f061e02f229ca55a4b96a19876d205e668a6
Vincent La <vincent.la@infoworks.io>
2019-06-30 13:41:32 -0700

    adding overlay background on party members

--- src/components/Wedding/wedding-party-member.jsx
+++ src/components/Wedding/wedding-party-member.jsx
@@ -5,6 +5,7 @@ import './wedding-party-member.css';
 function WeddingPartyMember({ index, viewport, name, content, src }) {
 	const [animationClass, setAnimationClass] = useState();
 	const listRef = useRef(null);
+	const isEven = index % 2 === 0;
 	useEffect(() => {
 		const rect = listRef.current.getBoundingClientRect();
 		const isVisible = !(
@@ -12,14 +13,17 @@ function WeddingPartyMember({ index, viewport, name, content, src }) {
 			Math.floor(100 - ((rect.bottom - viewport.height) / rect.height) * 100) < 20
 		  );
 		if (isVisible) {
-			setAnimationClass(index % 2 === 0 ? 'wedding-party-visible-left' : 'wedding-party-visible-right')
+			setAnimationClass(isEven ? 'wedding-party-visible-left' : 'wedding-party-visible-right')
 		} else {
 			setAnimationClass('not-visible');
 		}
 	})
 	return (
 		<li ref={listRef} className={`wedding-party-item ${animationClass}`}>
-			<h2>{name}</h2>
+			<div className="grey-overlay" />
+			<div className="wedding-party-item-content" style={{ justifyContent: isEven ? 'flex-start' : 'flex-end' }}>
+				<h2>{name}</h2>
+			</div>
 		</li>
 	)
 }
