ef13e0ba5c90b42b8a1311c9170c7488c0f4a51a
Vincent La <vincent.la@infoworks.io>
2019-06-30 12:50:12 -0700

    add splash for wedding party.  Add in animation for wedding party frames

---
+++ src/components/Wedding/wedding-party-member.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import './wedding-party-member.css';
+
+function WeddingPartyMember({ index, viewport }) {
+	const [animationClass, setAnimationClass] = useState();
+	const listRef = useRef(null);
+	useEffect(() => {
+		const rect = listRef.current.getBoundingClientRect();
+		const isVisible = !(
+			Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-(rect.height / 1)) * 100)) < 20 ||
+			Math.floor(100 - ((rect.bottom - viewport.height) / rect.height) * 100) < 20
+		  );
+		if (isVisible) {
+			setAnimationClass(index % 2 === 0 ? 'wedding-party-visible-left' : 'wedding-party-visible-right')
+		} else {
+			setAnimationClass('not-visible');
+		}
+	})
+	return (
+		<li ref={listRef} className={`wedding-party-item ${animationClass}`}>
+			Item 1
+		</li>
+	)
+}
+
+WeddingPartyMember.propTypes = {
+	viewport: PropTypes.object,
+	index: PropTypes.number.isRequired,
+}
+
+export default WeddingPartyMember;
--- src/components/Wedding/wedding.jsx
+++ src/components/Wedding/wedding.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import LazyImage from '../common/lazy-img-loader';
+import WeddingPartyMember from './wedding-party-member';
+import useWindowDimensions from '../hooks/window-size';
+import './wedding.css';
 
-const Wedding = () => (
-  <div className="non-splash-container">
-    <div style={{
-      display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '2rem',
-    }}
-    >
-      <div>Under Construction</div>
-      <i className="fas fa-tools fa-7x" />
-      <i className="fas fa-user-hard-hat" />
-    </div>
-
-  </div>
-);
+function Wedding() {
+	const { height, width } = useWindowDimensions();
+	const viewport = { height, width };
+	const src = '/splash_party.jpg'
+	let weddingParty = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8'];
+	weddingParty = weddingParty.map((person, index) => <WeddingPartyMember viewport={viewport} key={index} index={index}/>)
+	return (
+		<div className="section-container">
+			<div className="grey-overlay" />
+			<div style={{ display: 'flex', width: '100%', zIndex: 2, padding: '2rem', flexDirection: 'column' }}>
+				<h4 className="section-header">The Wedding Party</h4>
+				<LazyImage src={src} />
+				<ul className="wedding-party-list-container">
+					{weddingParty}
+				</ul>
+			</div>
+		</div>
+	);
+}
 
 export default Wedding;
